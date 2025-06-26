ba354e95f5d743eff0db42ad29277223d14de991
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-06-26 19:15:17 +0700

    feat(products): implement get endpoint for product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,7 +15,25 @@ export const getProducts = async (req, res) => {
 	}
 };
 
-export const getProduct = async (req, res) => {};
+export const getProduct = async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const product = await sql`
+      SELECT * FROM products WHERE id=${id}
+    `;
+
+		if (product.length === 0) {
+			return res.status(404).json({ success: false, message: "Product not found" });
+		}
+
+		res.status(200).json({ success: true, data: product[0] });
+	} catch (error) {
+		console.log("Error getProduct", error);
+
+		res.send(500).json({ success: false, message: "Internal server error" });
+	}
+};
 
 export const createProduct = async (req, res) => {};
 
