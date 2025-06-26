fb6f560c51aa92b257eaef8885845db244c075df
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-06-26 13:58:03 +0700

    Initial commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,11 @@
+import { sql } from "../config/database.js";
+
+export const getProducts = async (req, res) => {};
+
+export const getProduct = async (req, res) => {};
+
+export const createProduct = async (req, res) => {};
+
+export const updateProduct = async (req, res) => {};
+
+export const deleteProduct = async (req, res) => {};
---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+import { sql } from "./config/database.js";
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const { HOST, PORT } = process.env;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/api/products", productRoutes);
+
+async function initializeDatabase() {
+	try {
+		await sql`
+      CREATE TABLE IF NOT EXISTS products(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        image VARCHAR(100) NOT NULL,
+        price NUMERIC(9) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+	} catch (error) {
+		console.log("Error initializing database", error);
+	}
+}
+
+initializeDatabase().then(() => {
+	app.listen(PORT, HOST, () => {
+		console.log(`Server running at http://${HOST}:${PORT}`);
+	});
+});
