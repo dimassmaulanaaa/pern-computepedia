7baae6a2bcfd2494385ba57155f66b7048ee9e03
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-06-26 14:07:44 +0700

    feat(products): implement get endpoint for all products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,19 @@
 import { sql } from "../config/database.js";
 
-export const getProducts = async (req, res) => {};
+export const getProducts = async (req, res) => {
+	try {
+		const products = await sql`
+      SELECT * FROM products
+      ORDER BY created_at DESC
+    `;
+
+		res.status(200).json({ success: true, data: products });
+	} catch (error) {
+		console.log("Error getProducts", error);
+
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+};
 
 export const getProduct = async (req, res) => {};
 
