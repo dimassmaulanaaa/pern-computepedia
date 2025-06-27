c6fe6f4bb82420303d65322526c0aff5c5a98ff5
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-06-27 18:06:29 +0700

    feat: integrate arcjet

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 
+import { aj } from "./lib/arcjet.js";
 import { sql } from "./config/database.js";
 import productRoutes from "./routes/productRoutes.js";
 
@@ -16,6 +17,36 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+	try {
+		const decision = await aj.protect(req, {
+			requested: 1,
+		});
+
+		if (decision.isDenied()) {
+			if (decision.reason.isRateLimit()) {
+				res.status(429).json({ error: "Too many request" });
+			} else if (decision.reason.isBot()) {
+				res.status(403).json({ error: "Bot access denied" });
+			} else {
+				res.status(403).json({ error: "Forbidden" });
+			}
+			return;
+		}
+
+		if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+			res.status(403).json({ error: "Spoofed bot detected" });
+			return;
+		}
+
+		next();
+	} catch (error) {
+		console.log("Arcjet error", error);
+
+		next(error);
+	}
+});
 
 app.use("/api/products", productRoutes);
 
