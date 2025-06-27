e68b6a1e82e564008b6fbb7dd4d2067841e91832
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-06-27 11:12:29 +0700

    feat(products): implement post endpoint for create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -35,7 +35,27 @@ export const getProduct = async (req, res) => {
 	}
 };
 
-export const createProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
+	const { name, price, image } = req.body;
+
+	if (!name || !price || !image) {
+		return res.status(400).json({ success: false, message: "All fields are required" });
+	}
+
+	try {
+		const createdProduct = await sql`
+      INSERT INTO products (name, price, image)
+      VALUES (${name}, ${price}, ${image})
+      RETURNING *
+    `;
+
+		res.status(201).json({ success: true, data: createdProduct[0] });
+	} catch (error) {
+		console.log("Error createProducts", error);
+
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+};
 
 export const updateProduct = async (req, res) => {};
 
