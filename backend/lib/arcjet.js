14c23327615089208081cec8f8d397783fc8559e
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-06-27 15:11:23 +0700

    feat: initialize arcjet

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,24 @@
+import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { ARCJET_KEY } = process.env;
+
+export const aj = arcjet({
+	key: ARCJET_KEY,
+	characteristics: ["ip.src"],
+	rules: [
+		shield({ mode: "LIVE" }),
+		detectBot({
+			mode: "LIVE",
+			allow: ["CATEGORY:SEARCH_ENGINE"],
+		}),
+		tokenBucket({
+			mode: "LIVE",
+			refillRate: 5,
+			interval: 10,
+			capacity: 10,
+		}),
+	],
+});
