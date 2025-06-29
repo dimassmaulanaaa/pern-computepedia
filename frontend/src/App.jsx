a961c379e5263525677c6dc908621f2e24afaeba
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-06-29 15:11:20 +0700

    feat: implement theme switching functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
+import { useThemeStore } from "./store/useThemeStore";
+
 import Navbar from "./components/Navbar";
 
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 
 function App() {
+	const { theme } = useThemeStore();
 	return (
-		<div className="min-h-screen bg-base-200 transition-colors duration-300">
+		<div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
 			<Navbar />
 
 			<Routes>
---
+++ frontend/src/constants/index.js
@@ -0,0 +1,37 @@
+export const THEMES = [
+	{
+		name: "business",
+		label: "Business",
+		colors: ["#1C4E80", "#0091D5", "#7DB9DE"],
+	},
+	{
+		name: "cyberpunk",
+		label: "Cyberpunk",
+		colors: ["#FF00FF", "#00FFFF", "#FF7598"],
+	},
+	{
+		name: "dracula",
+		label: "Dracula",
+		colors: ["#282A36", "#BD93F9", "#FF79C6"],
+	},
+	{
+		name: "luxury",
+		label: "Luxury",
+		colors: ["#171618", "#B6862D", "#E2C697"],
+	},
+	{
+		name: "night",
+		label: "Night",
+		colors: ["#0F172A", "#334155", "#64748B"],
+	},
+	{
+		name: "retro",
+		label: "Retro",
+		colors: ["#e2d5bc", "#ef9995", "#a4cbb4"],
+	},
+	{
+		name: "valentine",
+		label: "Valentine",
+		colors: ["#E96D7B", "#FF8FAB", "#FFB3C6"],
+	},
+];
