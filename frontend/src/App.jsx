a8b6fa94e2dd062b406ccd701f06a95de3cee9e1
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-07-11 21:04:29 +0700

    refactor: relocate layout components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import PublicLayout from "./pages/PublicLayout";
-import ProtectedLayout from "./pages/ProtectedLayout";
+import PublicLayout from "./components/PublicLayout";
+import ProtectedLayout from "./components/ProtectedLayout";
+
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
@@ -13,15 +14,15 @@ function App() {
 		<>
 			<Routes>
 				{/* PUBLIC ROUTES */}
-				<Route element={<PublicLayout />}>
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/register" element={<RegisterPage />} />
+				<Route path="/" element={<PublicLayout />}>
+					<Route path="login" element={<LoginPage />} />
+					<Route path="register" element={<RegisterPage />} />
 				</Route>
 
 				{/* PROTECTED ROUTES */}
-				<Route element={<ProtectedLayout />}>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/product/:id" element={<EditProductPage />} />
+				<Route path="/" element={<ProtectedLayout />}>
+					<Route index element={<HomePage />} />
+					<Route path="edit-product/:id" element={<EditProductPage />} />
 				</Route>
 			</Routes>
 
